cdcbfdb405072ee62089557e6afabeed8af952c9
Fazal00007 <111645285+Fazal00007@users.noreply.github.com>
2023-04-24 23:22:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 WAIFUVID = [ 
-            "" 
+            "https://telegra.ph/file/4aa0c68c9ff4df2a75790.mp4"
+            "https://telegra.ph/file/ce4181abc0bda8c67ba71.mp4" 
+           
            ]
 
 SANJICOOK = [ 
-             "" 
+             "https://telegra.ph/file/639514a0c538160de520f.mp4" 
             ]
 
 @app.get("/waifuvid")
